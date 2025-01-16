fcc60807d76836e315a5ed29785f24484aca7444
Guy Kongthong <guykongthongcmu@gmail.com>
2025-01-16 23:28:40 +0700

    Update studentID.java

--- studentID.java
+++ studentID.java
@@ -1,9 +1,17 @@
+
+//672110545 Virawit Kongthong ADT Tutorial 3
+
 public class studentID {
     public static void main(String[] args) {
-        stack studentIDArray = new stack(5);
+        stack studentIDArray = new stack(9);
 
+        int studentID = 672115045;
+        String studentIDString = Integer.toString(studentID);
 
-        studentIDArray.push(672115045);
+        for (int i = 0; i < 9; i++) {
+            studentIDArray.push(Character.getNumericValue(studentIDString.charAt(i)));
+        }
+
         studentIDArray.peek();
         studentIDArray.pop();
         studentIDArray.pop();
