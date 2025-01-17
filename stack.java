52dda623969f8cdf6d3130219a6d4e6b5c745749
Guy Kongthong <guykongthongcmu@gmail.com>
2025-01-17 15:23:01 +0700

    Update stack.java

--- stack.java
+++ stack.java
@@ -1,17 +1,17 @@
 //672110545 Virawit Kongthong ADT Tutorial 3
 
 public class stack{
-    private static int[] stackArray;
+    private static studentClass[] stackArray;
     private static int top;
     private static int capacity; 
         
     public stack(int capacityf) {
         capacity = capacityf;
-        stackArray = new int[capacity];
+        stackArray = new studentClass[capacity];
         top = -1;
     }
         
-    public void push(int element) {
+    public void push(studentClass element) {
         if (isFull()) {
         System.out.println("Stack overflow");
     } else {
@@ -19,10 +19,10 @@ public class stack{
     }
     }
 
-    public int pop() {
+    public studentClass pop() {
         if (isEmpty()) {
             System.out.println("Stack underflow!");
-            return -1;
+            return null;
         } else {
             return stackArray[top--];
         }
@@ -36,8 +36,12 @@ public class stack{
         return top == capacity;
     }
 
-    public void peek() {
-        System.out.println(stackArray[top]);
+    public studentClass peek() {
+        return stackArray[top];
+    }
+
+    public int max() {
+        return top + 1;
     }
 
     public static int capacity() {
